02824f64efa608e821b137c9cbb10920d0c4757d
joaocsf <noobcsf@hotmail.com>
2017-04-16 19:44:53 +0100

    Added Powerline Theme

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ sudo pip install powerline-gitstatus
 
 git clone https://github.com/VundleVim/Vundle.vim.git ./.vim/bundle/Vundle.vim
 wget https://github.com/FortAwesome/Font-Awesome/blob/master/fonts/fontawesome-webfont.ttf
-mv ./~fonts/fontawesome-webfont.ttf
+mkdir ~/.fonts
+
+mv fontawesome-webfont.ttf ~/.fonts/
+
 
 
